7b425317c5712e7c31b9404497d7c10f9c2bdb90
Matt Turner, PhD <maturner01@gmail.com>
2026-05-29 19:45:27 -0700

    basic portaling working

--- archportal.sh
+++ archportal.sh
@@ -1,33 +1,56 @@
-#!/usr/bin/env  sh
+#!/usr/bin/env bash
 #
 # Author: Matt Turner <https://mat.phd>
 # Date: 27 May 2026
 #
 
+
 set -euo pipefail
 
+
+# Require sudo
+sudo -v || exit 1
+
+
+# Load definitions of the physical and mount locations of Arch
 source set_env.sh
 
-source portal_art.sh
 
-# Double check that relevant dirs are 
-for dir in "" dev proc sys run; do
-	umount --bind "$ARCH_MOUNT/$dir" || true
-done
+# Mount Arch Linux root
+printf "*** Mounting Arch Linux partition if not mounted..."
+mountpoint -q $ARCH_MOUNT || sudo mount $ARCH_PHYSICAL $ARCH_MOUNT
+printf "Done. ***\n\n"
+
+
+# Print portal art...
+clear
+envsubst < portal_art.txt 
+
+
+# ...and prompt user to enter
+read -p "Step through the portal? [Y/n] " answer
+answer=${answer:-Y}
+case "$answer" in [Yy]|[Yy][Ee][Ss])
+		printf "\n\nEnjoy!\n\n"
+		;;
+	*)
+		printf "\n\nNo? Maybe next time...\n\n"
+		exit 1
+		;;
+esac
+
 
 # Shut down Ubuntu's graphics process, GNOME, etc
-systemctl stop gdm || true
+printf "*** Stopping GDM... "
+sudo systemctl stop gdm || true
+printf "Done. Bye! ***\n\n"
 
-mount $ARCH_PHYSICAL $ARCH_MOUNT
 
-for dir in dev proc sys run; do
-	mount --bind "$ARCH_MOUNT/$dir" || true
-done
+# Log in to Arch Linux 
+sudo arch-chroot $ARCH_MOUNT /bin/bash -lc 'su - kirby && fish'
 
-sudo arch-chroot $ARCH_MOUNT
 
-for dir in dev proc sys run; do
-	umount --bind "$ARCH_MOUNT/$dir" || true
-done
+#===== POST-ARCH, BACK IN UBUNTU =====##
 
-umount $ARCH_MOUNT
+# When done in Arch, unmount its root
+sudo umount $ARCH_MOUNT || true
--- chroot_existing_arch.sh
+++ chroot_existing_arch.sh
@@ -1,1 +1,23 @@
-sudo chroot /mnt/arch/ /bin/bash
+#!/usr/bin/env bash
+#
+# Author: Matt Turner <https://mat.phd>
+# Date: 27 May 2026
+#
+
+set -euo pipefail
+
+
+# Require sudo
+sudo -v || exit 1
+
+# Load definitions of the physical and mount locations of Arch
+source set_env.sh
+printf "*** Mounting Tegra/Ubuntu kernel partitions if not mounted... "
+for dir in dev proc sys run; do
+	MOUNT_POINT="$ARCH_MOUNT/$dir"
+	sudo mkdir -p $MOUNT_POINT
+	mountpoint -q $MOUNT_POINT || sudo mount --bind "/$dir" $MOUNT_POINT
+done
+printf "...Done. ***\n\n"
+
+sudo chroot /mnt/arch/
